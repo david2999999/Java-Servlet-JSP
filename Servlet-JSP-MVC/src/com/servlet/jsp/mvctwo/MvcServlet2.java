e832e4c8456f0fc7e0f1804f5582f0c71c324023
David Jiang <djiang86@binghamton.edu>
2018-06-12 12:40:18 +0000

    servlet to retrieve the data and sent it to JSP

---
+++ Servlet-JSP-MVC/src/com/servlet/jsp/mvctwo/MvcServlet2.java
@@ -0,0 +1,66 @@
+package com.servlet.jsp.mvctwo;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MvcServlet2
+ */
+@WebServlet("/MvcServlet2")
+public class MvcServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MvcServlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// get the student data from the helper class (StudentDataUtil.java)
+		List<Student> students = StudentDataUtil.getStudents();
+		
+		// add the students to the request object
+		request.setAttribute("student_list", students);
+		
+		// get request dispatcher
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/view_students_two.jsp");
+		
+		// now forward to jsp
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
